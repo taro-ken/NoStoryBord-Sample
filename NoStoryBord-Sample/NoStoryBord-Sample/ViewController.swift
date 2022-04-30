32b064a38762d8a3c1b226bfbf865f107367999e
kentaro kimoto <exiler2411k@gmail.com>
2022-04-30 13:12:15 +0900

    init

--- NoStoryBord-Sample/NoStoryBord-Sample/ViewController.swift
+++ NoStoryBord-Sample/NoStoryBord-Sample/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let view1 = UIView()
+    let view2 = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view1.backgroundColor = .blue
+        view.addSubview(view1)
+        view1.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        view2.backgroundColor = .red
+        view.addSubview(view2)
+        view2.snp.makeConstraints{
+            $0.size.equalTo(100)
+            $0.center.equalToSuperview()
+        }
+        
+        
     }
-
-
+    
+    
+    
 }
 
